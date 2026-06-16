27035142edce15a224b76d9e3d636ba4438ddbc0
atremonic <rtm4bk@gmail.com>
2026-06-16 16:39:51 -0700

    Update footer link target to GitHub

--- src/App.tsx
+++ src/App.tsx
@@ -186,7 +186,7 @@ function App() {
       )}
 
       <footer>
-        <a href="https://artemonic.github.io/pathifier/">Pathifier</a> © 2026 by <a href="https://www.printables.com/@artemonic">artemonic</a> is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>
+        <a href="https://artemonic.github.io/pathifier/">Pathifier</a> © 2026 by <a href="https://github.com/artemonic">artemonic</a> is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>
         <img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" alt="" style={{ maxWidth: '1em', maxHeight: '1em', marginLeft: '.2em' }} />
         <img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" alt="" style={{ maxWidth: '1em', maxHeight: '1em', marginLeft: '.2em' }} />
         <img src="https://mirrors.creativecommons.org/presskit/icons/sa.svg" alt="" style={{ maxWidth: '1em', maxHeight: '1em', marginLeft: '.2em' }} />
